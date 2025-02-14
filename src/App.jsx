9e82586ef925deb297008bb6f10c45a96ca14c27
Adnan Ali <adnanshah7272.as@gmail.com>
2025-02-14 12:00:32 +0500

    Inital upload

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>The React Journey Begins</h1>
+      <p>A New Journey of learning React JS Developement begins, Learning to build something new.</p>
+    </div>
+  )
+}
+
+export function Test() {
+  return (
+    <div>
+      <h1>The Test Function</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit officia ipsum aliquam ipsa neque </p>
+    </div>
+  )
+}
+
+export function Button() {
+  return (
+    <div>
+     <button class="primary-button" type="button">Click Me!</button>
+
+    </div>
+  )
+}
+
+export default App
